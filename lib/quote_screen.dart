e6dee6fc45b194b13a8fadee62036fb158a18e56
raghav042 <raghvshukla042@gmail.com>
2023-08-16 22:20:27 +0530

    add quicksand font and author name

--- lib/quote_screen.dart
+++ lib/quote_screen.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/cupertino.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class QuoteScreen extends StatelessWidget {
   const QuoteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(30.0),
-      child: Center(
-        child: Text(
-          "If you can't be the sun be the star, Be the best of whatever you are",
-          style: GoogleFonts.quicksand(
-            fontSize: 28,
-            fontWeight: FontWeight.w500,
+    return const Padding(
+      padding: EdgeInsets.all(30.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "If you can't be the sun be the star, Be the best of whatever you are",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w500,
+            ),
           ),
-        ),
+          SizedBox(height: 40),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Text(
+              "- Author Name",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
