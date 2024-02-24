8df3de76fe1ba2a0306539061126ec4f7f224e1e
Gauri <117257419+Gauri24982003@users.noreply.github.com>
2024-02-24 18:52:01 +0530

    Update

--- lib/change_theme.dart
+++ lib/change_theme.dart
@@ -1,89 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-class ThemeCard extends StatelessWidget {
+class ThemeCard extends StatefulWidget {
+  @override
+  State<ThemeCard> createState() => _ThemeCardState();
+}
+
+class _ThemeCardState extends State<ThemeCard> {
   List<String> imagePath = [
     "assets/images/nature.jpg",
+    "assets/images/plant.jpg",
+    "assets/images/plant2.jpg",
+    "assets/images/sky.jpg",
   ];
+  int selectedBackgroundIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Themes'),
-      ),
-      body: Column(
-        children: [
+        appBar: AppBar(
+          title: const Text('Themes'),
+        ),
+        body: Column(children: [
           Wrap(
-            spacing: 10.0, // Horizontal spacing between cards
-            runSpacing: 10.0, // Vertical spacing between rows
-            children: [
-              //TODO: create theme for card and make them reusable
-              Card(
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Image.asset(
-                    "assets/images/nature.jpg",
-                    height: 150,
-                    width: 120,
-                    fit: BoxFit.cover,
+              spacing: 10.0, // Horizontal spacing between cards
+              runSpacing: 10.0, // Vertical spacing between rows
+              children: List.generate(
+                imagePath.length,
+                (index) => GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedBackgroundIndex = index;
+                    });
+                    Navigator.pop(context, imagePath[index]);
+                  },
+                  child: Card(
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.asset(
+                        imagePath[index],
+                        height: 150,
+                        width: 120,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              Card(
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Image.asset(
-                    "images/plant.jpg",
-                    fit: BoxFit.cover,
-                    height: 150,
-                    width: 120,
-                    // Replace with your image path
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Image.asset(
-                    "images/plant2.jpg",
-                    fit: BoxFit.cover, // Replace with your image path
-                    height: 150,
-                    width: 120,
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Image.asset(
-                    "images/sky.jpg",
-                    fit: BoxFit.cover, // Replace with your image path
-                    height: 150,
-                    width: 120,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+              ))
+        ]));
   }
 }
