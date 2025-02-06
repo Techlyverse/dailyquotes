1352590e56cb82270b52e830bb7c7048959ef4d6
Raghav Shukla <raghav.shukla@neosoftmail.com>
2025-02-06 21:21:13 +0530

    update state management

---
+++ lib/provider/bg_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bg_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$bgNotifierHash() => r'ff028ee1ea4ca50b0c3713709c0db28b8dc2a325';
+
+/// See also [BgNotifier].
+@ProviderFor(BgNotifier)
+final bgNotifierProvider =
+    AutoDisposeNotifierProvider<BgNotifier, BoxDecoration>.internal(
+  BgNotifier.new,
+  name: r'bgNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$bgNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BgNotifier = AutoDisposeNotifier<BoxDecoration>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
