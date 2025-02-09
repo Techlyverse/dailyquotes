8747df4a7c088c4731b7346524aa890518075a9f
raghav042 <raghvshukla042@gmail.com>
2025-02-09 13:16:39 +0530

    add fonts

---
+++ lib/provider/font_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'font_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fontNotifierHash() => r'40bb27296602a898e56949863cced034f1714909';
+
+/// See also [FontNotifier].
+@ProviderFor(FontNotifier)
+final fontNotifierProvider =
+    AutoDisposeNotifierProvider<FontNotifier, int>.internal(
+  FontNotifier.new,
+  name: r'fontNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$fontNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FontNotifier = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
