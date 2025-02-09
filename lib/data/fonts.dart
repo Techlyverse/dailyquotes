95a76278f3f5a0a5b401ec791d7af8f88abfcf47
raghav042 <raghvshukla042@gmail.com>
2025-02-09 13:18:35 +0530

    add fonts

--- lib/data/fonts.dart
+++ lib/data/fonts.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-List<TextStyle> fonts = [
-  GoogleFonts.ubuntu(),
-  GoogleFonts.poppins(),
-  GoogleFonts.aBeeZee(),
-  GoogleFonts.content(),
+final List<TextStyle> fonts = [
+  GoogleFonts.ubuntu(fontSize: 22),
+  GoogleFonts.poppins(fontSize: 22),
+  GoogleFonts.aBeeZee(fontSize: 22),
+  GoogleFonts.content(fontSize: 22),
+  GoogleFonts.abyssinicaSil(fontSize: 22),
+  GoogleFonts.aDLaMDisplay(fontSize: 22),
+  GoogleFonts.agdasima(fontSize: 22),
+  GoogleFonts.cabin(fontSize: 22),
 ];
